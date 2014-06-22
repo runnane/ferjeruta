c1592c6d291d0a6157559073c2314e1c55353709
runnane <psax@runnane.com>
2014-06-22 16:30:40 -0400

    undo piwik change, typo

--- ferjeruta.js
+++ ferjeruta.js
@@ -167,13 +167,11 @@ $(document)
 		
 		// Embed Piwik loading code (only for production site)
 		if(_fr.Settings.PiwikEnabled == 1){
-            /*
 			var u=(("https:" == document.location.protocol) ? "https" : "http") + "://projects.runnane.no/piwik/";
-			_paq.push(["sestTrackerUrl", u+"piwik.php"]);
+			_paq.push(["setTrackerUrl", u+"piwik.php"]);
 			_paq.push(["setSiteId", "1"]);
 			var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
 			g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
-			*/
 		}
 	
 		
